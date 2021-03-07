3c349035b93cd7a1bb8b7c776adbfe91792d379d
Irfan Ullah <irfanullah.khan@dynamologic.com>
2021-03-08 01:38:07 +0500

    Inbox Completed

---
+++ database/migrations/2021_03_07_130648_add_status_to_proposals_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStatusToProposalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('proposals', function (Blueprint $table) {
+            $table->enum('status',['pending','started','rejected'])->default('pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('proposals', function (Blueprint $table) {
+            //
+        });
+    }
+}
