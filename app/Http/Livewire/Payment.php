f966623950367f5aeff0087a67c5b14a7d59f173
IU KHAN <iu.khan53@gmail.com>
2021-07-06 21:09:59 +0500

    payment completed

--- app/Http/Livewire/Payment.php
+++ app/Http/Livewire/Payment.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\BuyerProposal;
 use Livewire\Component;
 
 class Payment extends Component
@@ -14,7 +15,14 @@ class Payment extends Component
     public $month;
     public $year;
     public $cvc;
-    public $loading = true;
+    public $loading = false;
+    protected $rules = [
+        'card_number' => ['required','string','max:16','min:16'],
+        'name' => ['required','string'],
+        'month' => ['required'],
+        'year' => ['required'],
+        'cvc' => ['required','integer'],
+    ];
 
     public function render()
     {
@@ -22,6 +30,11 @@ class Payment extends Component
     }
 
     public function processPayment(){
-
+        $this->validate($this->rules);
+        $order = BuyerProposal::find($this->order_id);
+        $order->payment_status = 'paid_by_buyer';
+        $order->save();
+        sleep(3);
+        $this->redirect(route('orders'));
     }
 }
--- resources/views/livewire/payment.blade.php
+++ resources/views/livewire/payment.blade.php
@@ -5,8 +5,7 @@
 </x-slot>
 <div class="container mx-auto">
     <!-- component -->
-    @if($loading)
-        <div class="flex justify-center items-center space-x-1 text-gray-700 mt-3">
+        <div wire:loading class="flex justify-center items-center space-x-1 text-gray-700 mt-3">
             <svg fill='none' class="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns='http://www.w3.org/2000/svg'>
                 <path clip-rule='evenodd'
                       d='M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z'
@@ -14,8 +13,7 @@
             </svg>
             <div>Processing ...</div>
         </div>
-    @else
-        <div class="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 pb-10 pt-16">
+        <div wire:loading.remove class="min-w-screen min-h-screen bg-gray-200 flex items-center justify-center px-5 pb-10 pt-16">
             <div class="w-full mx-auto rounded-lg bg-white shadow-lg p-5 text-gray-700" style="max-width: 600px">
                 <div class="w-full pt-1 pb-5">
 
@@ -26,16 +24,8 @@
                 <div class="mb-3 flex -mx-2">
                     <div class="px-2">
                         <label for="type1" class="flex items-center cursor-pointer">
-                            <input type="radio" class="form-radio h-5 w-5 text-indigo-500" name="type" id="type1"
-                                   checked>
+
                             <img src="https://leadershipmemphis.org/wp-content/uploads/2020/08/780370.png"
-                                 class="h-8 ml-3">
-                        </label>
-                    </div>
-                    <div class="px-2">
-                        <label for="type2" class="flex items-center cursor-pointer">
-                            <input type="radio" class="form-radio h-5 w-5 text-indigo-500" name="type" id="type2">
-                            <img src="https://www.sketchappsources.com/resources/source-image/PayPalCard.png"
                                  class="h-8 ml-3">
                         </label>
                     </div>
@@ -47,20 +37,22 @@
                                class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                                placeholder="John Smith" type="text"/>
                     </div>
+                    <x-jet-input-error for="name" class="mt-2"/>
                 </div>
                 <div class="mb-3">
                     <label class="font-bold text-sm mb-2 ml-1">Card number</label>
                     <div>
-                        <input wire:model="card_number"
+                        <input wire:model.defer="card_number"
                                class="w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                                placeholder="0000 0000 0000 0000" type="text"/>
                     </div>
+                    <x-jet-input-error for="card_number" class="mt-2"/>
                 </div>
                 <div class="mb-3 -mx-2 flex items-end">
                     <div class="px-2 w-1/2">
                         <label class="font-bold text-sm mb-2 ml-1">Expiration date</label>
                         <div>
-                            <select wire:model="month"
+                            <select wire:model.defer="month"
                                     class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
                                 <option value="01">01 - January</option>
                                 <option value="02">02 - February</option>
@@ -75,10 +67,11 @@
                                 <option value="11">11 - November</option>
                                 <option value="12">12 - December</option>
                             </select>
+                            <x-jet-input-error for="month" class="mt-2"/>
                         </div>
                     </div>
                     <div class="px-2 w-1/2">
-                        <select wire:model="year"
+                        <select wire:model.defer="year"
                                 class="form-select w-full px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors cursor-pointer">
                             <option value="2020">2020</option>
                             <option value="2021">2021</option>
@@ -91,23 +84,25 @@
                             <option value="2028">2028</option>
                             <option value="2029">2029</option>
                         </select>
+                        <x-jet-input-error for="year" class="mt-2"/>
+
                     </div>
                 </div>
                 <div class="mb-10">
                     <label class="font-bold text-sm mb-2 ml-1">Security code</label>
                     <div>
-                        <input wire:model="cvc"
+                        <input wire:model.defer="cvc"
                                class="w-32 px-3 py-2 mb-1 border-2 border-gray-200 rounded-md focus:outline-none focus:border-indigo-500 transition-colors"
                                placeholder="000" type="text"/>
                     </div>
+                    <x-jet-input-error for="cvc" class="mt-2"/>
                 </div>
                 <div>
                     <button wire:click="processPayment"
                         class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
-                        <i class="mdi mdi-lock-outline mr-1"></i> PAY {{$amount}} PKR
+                        <i class="mdi mdi-lock-outline mr-1"></i> PAY RS. {{$amount}}
                     </button>
                 </div>
             </div>
         </div>
-    @endif
 </div>
